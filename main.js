83af8bb0854a839ee782bdcf1a00efbdcbad9faa
Roche Kollie <35652808+rochekollie@users.noreply.github.com>
2021-11-18 14:12:21 -0500

    Created Content Draft
    Added draft contents to the page.

---
+++ main.js
@@ -0,0 +1,5 @@
+let bookTitle = document.getElementsByTagName('h1')[0];
+
+bookTitle.addEventListener('click', function () {
+	window.location.href = 'https://www.google.com/books/edition/Why_Nations_Fail/yIV_NMDDIvYC?hl=en';
+});
