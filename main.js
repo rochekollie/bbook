fc3d322fc53c26f22282d81356320397f25e38a3
Roche Kollie <35652808+rochekollie@users.noreply.github.com>
2021-11-18 14:23:19 -0500

    Added Style to <h1>
    Added CSS styling to <h1> using JavaScript.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let bookTitle = document.getElementsByTagName('h1')[0];
 
+bookTitle.addEventListener('mouseover', function () {
+	bookTitle.classList.add('book-title');
+});
+
 bookTitle.addEventListener('click', function () {
 	window.location.href = 'https://www.google.com/books/edition/Why_Nations_Fail/yIV_NMDDIvYC?hl=en';
 });
